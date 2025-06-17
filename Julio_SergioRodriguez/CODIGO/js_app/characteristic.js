0fe3dd8e9ceceb4f389ab9bb42170119e9bd7564
seryio2004 <86850969+seryio2004@users.noreply.github.com>
2025-06-17 12:22:29 +0200

    Link characteristic entity to index

--- Julio_SergioRodriguez/CODIGO/js_app/characteristic.js
+++ Julio_SergioRodriguez/CODIGO/js_app/characteristic.js
@@ -1,7 +1,9 @@
 class characteristic extends EntidadAbstracta {
     constructor() {
-        this.entidad="characteristic";
-        this.estructura= characteristic_estructura;
-
+        super();
+        this.entidad = 'characteristic';
+        this.estructura = estructura_characteristic;
+        this.columnasamostrar = this.estructura.columnas_visibles_tabla;
+        this.datosespecialestabla = this.estructura.columnas_modificadas_tabla;
     }
 }
